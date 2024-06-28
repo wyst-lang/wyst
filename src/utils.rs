8cbe87d59c886bffb4868175d4ad546a10acb86d
Leo Dev <klesti.selimay@icloud.com>
2024-06-28 14:57:55 +0200

    rewrite 1

---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum ProblemType {
+    VariableNotFound,
+    FileNotFound,
+    HeaderSyntaxError,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Problem {
+    //pub code: u32,
+    pub problem_type: ProblemType,
+    pub problem_msg: String,
+}
